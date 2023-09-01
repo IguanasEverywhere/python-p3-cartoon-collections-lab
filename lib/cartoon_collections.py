170db19b14f57162f47f00aa8acfb17bfaf30e3b
Scott Schwab <scottschwab86@gmail.com>
2023-09-01 11:29:15 -0500

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for idx, dwarf in enumerate(dwarves):
+        print(f'{idx + 1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [f'{call.capitalize()}!' for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+    for item in items:
+        if item in cheeses:
+            return item
+    return None
